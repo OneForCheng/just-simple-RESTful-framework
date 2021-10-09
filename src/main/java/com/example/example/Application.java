227fde840f798d81d99cdd457558c67db842cd49
Huang Cheng <chuang@thoughtworks.com>
2021-10-09 22:52:32 +0800

    [huang cheng] feat: add `Application` to bootstrap `RESTApplication`

---
+++ src/main/java/com/example/example/Application.java
@@ -0,0 +1,9 @@
+package com.example.example;
+
+import com.example.JustSimpleRESTfulFramework.RESTApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        new RESTApplication().bootstrap(Application.class);
+    }
+}
