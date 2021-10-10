18888818aa6cb1fcda1416db6b690fd799d190b7
Huang Cheng <chuang@thoughtworks.com>
2021-10-10 11:16:16 +0800

    [huang cheng] feat: get all resources from `@RESTResource`

--- src/main/java/com/example/JustSimpleRESTfulFramework/resource/ResourceResolver.java
+++ src/main/java/com/example/JustSimpleRESTfulFramework/resource/ResourceResolver.java
@@ -1,7 +1,25 @@
 package com.example.JustSimpleRESTfulFramework.resource;
 
-public class ResourceResolver {
-    public ResourceResolver(Class<?> bootstrapClass) {
+import com.example.JustSimpleRESTfulFramework.annotation.RESTResource;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ResourceResolver {
+    Set<Class<?>> resources;
+
+    public ResourceResolver(Class<?> bootstrapClass) {
+        resources = getRESTResources(bootstrapClass);
+    }
+
+    Set<Class<?>> getRESTResources(Class<?> bootstrapClass) {
+        Set<Class<?>> classes = Collections.synchronizedSet(new LinkedHashSet<>());
+        if (bootstrapClass.isAnnotationPresent(RESTResource.class)) {
+            RESTResource annotation = bootstrapClass.getAnnotation(RESTResource.class);
+            classes.addAll(Arrays.asList(annotation.value()));
+        }
+        return classes;
     }
 }
