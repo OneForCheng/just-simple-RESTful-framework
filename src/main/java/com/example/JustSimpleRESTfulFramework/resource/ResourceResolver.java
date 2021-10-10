b67f3f9f39704fb690ab233b727e3330d0f86ada
Huang Cheng <chuang@thoughtworks.com>
2021-10-10 17:43:42 +0800

    [huang cheng] feat: get return result of resource by resolve uri

--- src/main/java/com/example/JustSimpleRESTfulFramework/resource/ResourceResolver.java
+++ src/main/java/com/example/JustSimpleRESTfulFramework/resource/ResourceResolver.java
@@ -4,9 +4,11 @@ import com.example.JustSimpleRESTfulFramework.annotation.GET;
 import com.example.JustSimpleRESTfulFramework.annotation.Path;
 import com.example.JustSimpleRESTfulFramework.annotation.RESTResource;
 import com.example.JustSimpleRESTfulFramework.model.ResponseResult;
+import com.thoughtworks.InjectContainer.InjectContainer;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.*;
@@ -14,10 +16,12 @@ import java.util.stream.Collectors;
 
 public class ResourceResolver {
     private final Map<Class<?>, List<String>> resources;
+    private final InjectContainer injectContainer;
     private final String PATH_SEPARATOR = "/";
 
     public ResourceResolver(Class<?> bootstrapClass) {
         resources = getRESTResources(bootstrapClass);
+        injectContainer = new InjectContainer();
     }
 
     private Map<Class<?>, List<String>> getRESTResources(Class<?> bootstrapClass) {
@@ -63,12 +67,52 @@ public class ResourceResolver {
         });
     }
 
+    public void populateResponseResult(ResponseResult responseResult, String url, Class<?> resource) throws InvocationTargetException, IllegalAccessException {
+        Object resourceInstance = injectContainer.getInstance(resource);
+        resolveReturnResultOfResource(responseResult, url, PATH_SEPARATOR, resource, resourceInstance);
+    }
+
+    public void resolveReturnResultOfResource(ResponseResult responseResult, String targetUrl, String parentPath, Class<?> resource, Object resourceInstance) throws InvocationTargetException, IllegalAccessException {
+        String newParentPath;
+        if (resource.isAnnotationPresent(Path.class)) {
+            newParentPath = combinePath(parentPath, getFormattedPath(resource.getAnnotation(Path.class).value()));
+        } else {
+            newParentPath = parentPath;
+        }
+        List<Method> publicMethods = Arrays.stream(resource.getMethods()).filter(method -> Modifier.isPublic(method.getModifiers())).collect(Collectors.toList());
+        for (Method method : publicMethods) {
+            if (method.isAnnotationPresent(GET.class)) {
+                if (method.isAnnotationPresent(Path.class)) {
+                    String formattedPath = getFormattedPath(method.getAnnotation(Path.class).value());
+                    String url =  combinePath(newParentPath, formattedPath);
+                    if (url.equals(targetUrl)) {
+                        Object result = method.invoke(resourceInstance);
+                        responseResult.setResult(result);
+                        return;
+                    }
+                }
+                if (newParentPath.equals(targetUrl)) {
+                    Object result = method.invoke(resourceInstance);
+                    responseResult.setResult(result);
+                    return;
+                }
+            }
+            if (!method.isAnnotationPresent(GET.class) && method.isAnnotationPresent(Path.class)) {
+                String nextParentPath = combinePath(newParentPath, getFormattedPath(method.getAnnotation(Path.class).value()));
+                Object returnTypeInstance = method.invoke(resourceInstance);
+                resolveReturnResultOfResource(responseResult, targetUrl, nextParentPath, method.getReturnType(), returnTypeInstance);
+            }
+        }
+    }
+
     public ResponseResult resolveUri(String uri) {
         try {
             String url  = combinePath(PATH_SEPARATOR, getFormattedPath(uri.split("\\?")[0]));
             for (Map.Entry<Class<?>, List<String>> resource : resources.entrySet()) {
                 if (resource.getValue().contains(url)) {
-                    return new ResponseResult(OK, url);
+                    ResponseResult responseResult = new ResponseResult(OK, null);
+                    populateResponseResult(responseResult, url, resource.getKey());
+                    return responseResult;
                 }
             }
         } catch (Exception e) {
