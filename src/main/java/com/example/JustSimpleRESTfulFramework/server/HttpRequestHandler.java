377b04ebcb3e2caf9846096d776753fd8af8b81b
Huang Cheng <chuang@thoughtworks.com>
2021-10-16 08:26:20 +0800

    [huang cheng] feat: use `HttpHeaderNames` instead of `ResponseConfig`

--- src/main/java/com/example/JustSimpleRESTfulFramework/server/HttpRequestHandler.java
+++ src/main/java/com/example/JustSimpleRESTfulFramework/server/HttpRequestHandler.java
@@ -2,7 +2,6 @@ package com.example.JustSimpleRESTfulFramework.server;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.example.JustSimpleRESTfulFramework.config.ResponseConfig;
 import com.example.JustSimpleRESTfulFramework.model.ResponseResult;
 import com.example.JustSimpleRESTfulFramework.resource.RequestResolver;
 import io.netty.buffer.Unpooled;
@@ -43,10 +42,10 @@ public class HttpRequestHandler extends ChannelInboundHandlerAdapter {
     private void populateResponse(ChannelHandlerContext ctx, ResponseResult res, boolean isKeepAlive) {
         byte[] bytes = JSON.toJSONBytes(res.getResult(), SerializerFeature.EMPTY);
         FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, res.getStatus(), Unpooled.wrappedBuffer(bytes));
-        response.headers().set(ResponseConfig.CONTENT_TYPE, "text/json");
-        response.headers().setInt(ResponseConfig.CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/json");
+        response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
         if (isKeepAlive) {
-            response.headers().set(ResponseConfig.CONNECTION, ResponseConfig.KEEP_ALIVE);
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderNames.KEEP_ALIVE);
             ctx.write(response);
         } else {
             ctx.write(response).addListener(ChannelFutureListener.CLOSE);
