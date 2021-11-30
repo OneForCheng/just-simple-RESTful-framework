0f9ff21486dc36202b27103a8d475a5367854ba8
Huang Cheng <chuang@thoughtworks.com>
2021-11-30 22:12:42 +0800

    [huang cheng] feat: extract `Resource` and rename it to `TestDemo`

---
+++ src/test/java/com/example/JustSimpleRESTfulFramework/model/TestDemo.java
@@ -0,0 +1,17 @@
+package com.example.JustSimpleRESTfulFramework.model;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+import javax.ws.rs.GET;
+
+public class TestDemo {
+    @GET
+    public String nonResponse() {
+        return "test";
+    }
+
+    @GET
+    public ResponseResult response() {
+        return new ResponseResult(HttpResponseStatus.ACCEPTED, "test");
+    }
+}
--- src/test/java/com/example/JustSimpleRESTfulFramework/resource/composite/ResourceItemTest.java
+++ src/test/java/com/example/JustSimpleRESTfulFramework/resource/composite/ResourceItemTest.java
@@ -3,36 +3,21 @@ package com.example.JustSimpleRESTfulFramework.resource.composite;
 import com.example.JustSimpleRESTfulFramework.model.RequestEntity;
 import com.example.JustSimpleRESTfulFramework.model.ResourceNode;
 import com.example.JustSimpleRESTfulFramework.model.ResponseResult;
+import com.example.JustSimpleRESTfulFramework.model.TestDemo;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.GET;
-
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResourceItemTest {
-
-    public class Resource {
-        @GET
-        public String nonResponse() {
-            return "test";
-        }
-
-        @GET
-        public ResponseResult response() {
-            return new ResponseResult(HttpResponseStatus.ACCEPTED, "test");
-        }
-    }
-
-
     @Test
     void should_return_false_if_url_is_not_matched() throws NoSuchMethodException {
         FullHttpRequest httpRequest = new DefaultFullHttpRequest(HTTP_1_1, HttpMethod.GET, "/test");
-        ResourceNode resourceNode = new ResourceNode("/no-test", HttpMethod.GET, Resource.class.getMethod("nonResponse"));
+        ResourceNode resourceNode = new ResourceNode("/no-test", HttpMethod.GET, TestDemo.class.getMethod("nonResponse"));
         ResourceItem item = new ResourceItem(resourceNode);
         assertFalse(item.isMatch(RequestEntity.of(httpRequest)));
     }
@@ -40,7 +25,7 @@ class ResourceItemTest {
     @Test
     void should_return_false_if_http_method_is_not_matched() throws NoSuchMethodException {
         FullHttpRequest httpRequest = new DefaultFullHttpRequest(HTTP_1_1, HttpMethod.POST, "/test");
-        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, Resource.class.getMethod("nonResponse"));
+        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, TestDemo.class.getMethod("nonResponse"));
         ResourceItem item = new ResourceItem(resourceNode);
         assertFalse(item.isMatch(RequestEntity.of(httpRequest)));
     }
@@ -48,7 +33,7 @@ class ResourceItemTest {
     @Test
     void should_return_true_if_http_method_and_url_are_matched() throws NoSuchMethodException {
         FullHttpRequest httpRequest = new DefaultFullHttpRequest(HTTP_1_1, HttpMethod.GET, "/test");
-        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, Resource.class.getMethod("nonResponse"));
+        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, TestDemo.class.getMethod("nonResponse"));
         ResourceItem item = new ResourceItem(resourceNode);
         assertTrue(item.isMatch(RequestEntity.of(httpRequest)));
     }
@@ -56,9 +41,9 @@ class ResourceItemTest {
     @Test
     void should_wrap_response_to_result_if_object_given() throws NoSuchMethodException {
         FullHttpRequest httpRequest = new DefaultFullHttpRequest(HTTP_1_1, HttpMethod.GET, "/test");
-        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, Resource.class.getMethod("nonResponse"));
+        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, TestDemo.class.getMethod("nonResponse"));
         ResourceItem item = new ResourceItem(resourceNode);
-        ResponseResult result = item.resolve(new Resource(), RequestEntity.of(httpRequest));
+        ResponseResult result = item.resolve(new TestDemo(), RequestEntity.of(httpRequest));
         assertEquals(HttpResponseStatus.OK, result.getStatus());
         assertEquals("test", result.getResult());
     }
@@ -66,9 +51,9 @@ class ResourceItemTest {
     @Test
     void should_return_response_directly_if_response_given() throws NoSuchMethodException {
         FullHttpRequest httpRequest = new DefaultFullHttpRequest(HTTP_1_1, HttpMethod.GET, "/test");
-        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, Resource.class.getMethod("response"));
+        ResourceNode resourceNode = new ResourceNode("/test", HttpMethod.GET, TestDemo.class.getMethod("response"));
         ResourceItem item = new ResourceItem(resourceNode);
-        ResponseResult result = item.resolve(new Resource(), RequestEntity.of(httpRequest));
+        ResponseResult result = item.resolve(new TestDemo(), RequestEntity.of(httpRequest));
         assertEquals(HttpResponseStatus.ACCEPTED, result.getStatus());
         assertEquals("test", result.getResult());
     }
